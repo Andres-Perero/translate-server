e0bc47dae421f442a6ac218180c66fae4e37681e
Andres-Perero <andres.pereros@hotmail.com>
2024-05-01 16:20:12 -0500

    Añadir configuración de CORS al servidor Express

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const translate = require('translate-google');
-const cors = require('cors');
+const express = require("express");
+const bodyParser = require("body-parser");
+const translate = require("translate-google");
+const cors = require("cors");
 
 const app = express();
 
@@ -12,22 +12,22 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // Ruta GET '/hola' que responde con "Hola" en pantalla
-app.get('/hola', (req, res) => {
-    res.send('Hola');
+app.get("/hola", (req, res) => {
+  res.send("Hola");
 });
 
-app.post('/translate', async (req, res) => {
-    const { text, target_language } = req.body;
+app.post("/translate", async (req, res) => {
+  const { text, target_language } = req.body;
 
-    try {
-        const translation = await translate(text, { to: target_language });
-        res.json({ translation });
-    } catch (error) {
-        res.status(500).json({ error: 'Error en la traducción' });
-    }
+  try {
+    const translation = await translate(text, { to: target_language });
+    res.json({ translation });
+  } catch (error) {
+    res.status(500).json({ error: "Error en la traducción" });
+  }
 });
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Servidor API de traducción en ejecución en el puerto ${PORT}`);
+  console.log(`Servidor API de traducción en ejecución en el puerto ${PORT}`);
 });
